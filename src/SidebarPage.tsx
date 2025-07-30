bcb8e56810f4aa9aba80324d782dce9b54c3295f
Yogesh <yogesh@celestialsys.com>
2025-07-30 11:49:43 +0530

    toogle context for teams

--- src/SidebarPage.tsx
+++ src/SidebarPage.tsx
@@ -1,5 +1,11 @@
+'use client';
+
 import { ReactNode } from 'react';
 import { SidebarHeader, SidebarHeaderTitle, SidebarMain } from './SidebarHeader';
+import { useSidebar } from '../../ui/sidebar';
+import { Separator } from '../../ui/separator';
+import { Button } from '../../ui/button';
+import { ViewVerticalIcon } from '@radix-ui/react-icons';
 
 interface SidebarPageProps {
   title: string;
@@ -8,10 +14,18 @@ interface SidebarPageProps {
 }
 
 export function SidebarPage({ title, children, className }: SidebarPageProps) {
+  const { toggleSidebar } = useSidebar('right');
   return (
     <>
       <SidebarHeader>
         <SidebarHeaderTitle>{title}</SidebarHeaderTitle>
+        <div className='flex items-center h-full md:hidden'>
+        <Separator orientation='vertical' className='h-4' />
+        <Button variant='ghost' size='icon' onClick={toggleSidebar}>
+          <ViewVerticalIcon />
+          <span className='sr-only'>Toggle Sidebar</span>
+        </Button>
+      </div>
       </SidebarHeader>
       <SidebarMain className={className}>{children}</SidebarMain>
     </>
